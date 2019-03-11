9ec8f87e87c54c9ac879181864b4684c59999a3d
RyoTsune <47287773+RyoTsune@users.noreply.github.com>
2019-03-11 16:49:46 +0900

    first commit

---
+++ learnPHP.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+Practice PHP command
+</head>
+<body>
+<?php
+$so = 'This is apple';
+switch ($so)
+{
+case 'khong quan' :
+echo 'This is Vietnamese'
+break;
+case 'こんにちは' :
+echo 'This is Japanese'
+break;
+case 'This is apple' :
+echo 'This is English'
+break;
+default
+echo 'I do not understand'
+break;
+}
+?>
+</body>
+</html>
